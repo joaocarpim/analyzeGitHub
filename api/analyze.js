476440e310cfd6c86e5d1cb4f0c27156d4e9896a
joaocarpim <joaovacs2004@gmail.com>
2026-02-01 03:36:20 -0300

    fix: teste api

--- api/analyze.js
+++ api/analyze.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  console.log("ENV KEYS:", Object.keys(process.env));
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
